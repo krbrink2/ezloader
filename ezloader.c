8536812abcaa7d001dbba5871bc7099fffd40cea
Kyle Brinker <krbrink2@illinois.edu>
2016-03-12 16:33:07 -0600

    Added grout_t and element_t

--- ezloader.c
+++ ezloader.c
@@ -3,12 +3,35 @@
 #include <stdio.h>
 #include <GL/gl.h>
 
-ezloadCallList
+/* The plan:
+	This loader will only handle points, lines, and faces, and ignore the rest.
+
+	Create a group_t struct, with name, material name, number of vertices, vertex array, texture vertex array, normal array, and number of elements, and element_t array.
+
+	Create element_t struct, with type, and vertex index array.
+*/
+
+typedef struct group_t{
+	char[64] name;
+	char[64] matName;
+	int numVertices;
+	double[3] * vertices;
+	double[3] * textureVertices;
+	double[3] * normals;
+	int numElements;
+	element_t * elements;
+} grout_t;
+
+typedef struct element_t{
+	char type;
+	int[3] vertexIndices;
+}
+
+int ezloadCallList(GLint callListIndex, FILE *fp){
 	glPointSize(2.0);
 	glNewList(callListIndex, GL_COMPILE);
 	{
 		int line = 0;
-		bool drawingPropeller = false;
 		glPushMatrix();
 		glBegin(GL_TRIANGLE_FAN);//GL_POINTS);
 		while(!feof(fp)){
@@ -38,3 +61,4 @@ ezloadCallList
 		
 	}
 	glEndList();
+}
